ddb2030b950cf5f8dcc10d1baeb276b34563d78f
Grace Aranico <grace.aranico@gmail.com>
2022-01-19 07:58:52 -0800

    chore(create project):create project and add bootstrap css, icons

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Navbar() {
+    return (
+        <nav>
+            <div className="nav-logo">Grace Aranico</div>
+            {/* <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/">About</Link>
+                <Link to="/">Projects</Link>
+            </div> */}
+        </nav>
+    )
+}
+
+export default Navbar
